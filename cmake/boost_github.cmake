54d9277199427d17e6092b9a13cb1f76ad2e6c41
John Fremlin <john@fremlin.org>
2025-04-26 23:37:32 -0400

    New Boost version

--- cmake/boost_github.cmake
+++ cmake/boost_github.cmake
@@ -2,10 +2,10 @@ set(BOOST_INCLUDE_LIBRARIES thread filesystem system program_options)
 set(BOOST_ENABLE_CMAKE ON)
 
 include(FetchContent)
+set(BOOST_ENABLE_CMAKE ON)
 FetchContent_Declare(
   Boost
-  GIT_REPOSITORY https://github.com/boostorg/boost.git
-  GIT_TAG boost-1.80.0
-  GIT_SHALLOW TRUE
+  URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
+  URL_MD5 893b5203b862eb9bbd08553e24ff146a
 )
 FetchContent_MakeAvailable(Boost)
